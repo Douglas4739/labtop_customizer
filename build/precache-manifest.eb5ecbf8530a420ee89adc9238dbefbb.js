4d37162b1d6a271433d31ccf7507bbaa7173d769
DouglasEde <briancarter340@gmail.com>
2020-11-17 09:45:28 -0800

    build project for GH-Pages

---
+++ build/precache-manifest.eb5ecbf8530a420ee89adc9238dbefbb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7a373d3824a895c995a42bb140d2087",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e8d94aaa93314230632",
+    "url": "/static/css/main.72a41afd.chunk.css"
+  },
+  {
+    "revision": "a3189dd90bd7726e1ec5",
+    "url": "/static/js/2.6549ce60.chunk.js"
+  },
+  {
+    "revision": "6e8d94aaa93314230632",
+    "url": "/static/js/main.3120d435.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
